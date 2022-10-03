b14d8cbe794290b504dfd2dd893471716b023e05
Dell <Dell@DESKTOP-NKE8ADQ>
2022-10-03 14:19:30 +0200

    Add project files.

---
+++ SnakeLadderGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SnakeLadderGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(1143, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(212, 742);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 387);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 36);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.Font = new System.Drawing.Font("Lucida Calligraphy", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(54, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = global::SnakeLadderGame.Properties.Resources.maxresdefault;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(5, 121);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(206, 144);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::SnakeLadderGame.Properties.Resources.Snake_and_Ladder_Template5;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Location = new System.Drawing.Point(6, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1131, 744);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.BackgroundImage = global::SnakeLadderGame.Properties.Resources.bluePart;
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(76, 689);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(36, 52);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.BackgroundImage = global::SnakeLadderGame.Properties.Resources.redPart;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(6, 689);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(36, 52);
+            this.panel4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 741);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SnakeLadderGame/Form1.cs
@@ -0,0 +1,290 @@
+﻿using SnakeLadderGame.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SnakeLadderGame
+{
+    public partial class Form1 : Form
+    {
+        int p1Position,p2Position;
+        int p1Direction,p2Direction;
+        bool p1Turn;
+        int pressed;
+        int redDraw, blueDraw;
+        public Form1()
+        {
+            p1Position = 1;
+            p2Position = 1;
+
+            p2Direction = 1;
+            p1Direction = 1;
+          
+            pressed = 0;
+            redDraw= 0;
+            blueDraw= 0;
+            InitializeComponent();
+            textBox1.Text = "Red Draw";
+        }
+
+     
+       
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            // draw to see who starts
+            Random rand = new Random();
+            if (pressed == 0)
+            {
+
+                redDraw = rand.Next(1, 7);
+                panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("d" + redDraw);
+                textBox1.Text = "Blue Draw";
+                pressed++;
+            }
+            else if (pressed == 1)
+            {
+                blueDraw = rand.Next(1, 7);
+                while (blueDraw == redDraw)
+                {
+                    blueDraw = rand.Next(1, 7);
+
+                }
+                string wonText;
+                panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("d" + blueDraw);
+                if (redDraw > blueDraw)
+                {
+                    button1.Enabled = false;
+                    wonText = "Red Starts";
+                    p1Turn = true;
+                    Application.DoEvents();
+                    Thread.Sleep(600);
+                    panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("redStart");
+
+                }
+                else
+                {
+                    button1.Enabled = false;
+                    wonText = "Blue Starts";
+                    p1Turn = false;
+                    Application.DoEvents();
+                    Thread.Sleep(600);
+                    panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("blueStart");
+
+                }
+                Application.DoEvents();
+                Thread.Sleep(600);
+                panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("roll");
+                textBox1.Text = wonText;
+
+                button1.Enabled = true;
+               
+                pressed++;
+            }
+            // logic for the game
+            else
+            {
+                int diceValue = rand.Next(1, 7);
+                panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("d" + diceValue);
+                button1.Enabled = false;
+                bool won;
+                // check for the turn
+                if (p1Turn)
+                {
+
+                    won = move(diceValue, ref p1Position, ref p1Direction, panel4);
+                    if (won)
+                    {
+                        textBox1.Text = "Red Won!";
+                        panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("redWon");
+                        button1.Enabled = false;
+                    }
+                    else
+                        textBox1.Text = "Blue Turn";
+                    p1Turn = false;
+                }
+                else
+                {
+                    won = move(diceValue, ref p2Position, ref p2Direction, panel5);
+                    if (won)
+                    {
+                        textBox1.Text = "Blue Won";
+                        panel3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("blueWon");
+                        button1.Enabled = false;
+
+                    }
+                    else
+                        textBox1.Text = "Red Turn";
+                    p1Turn = true;
+                }
+                button1.Enabled = true;
+            }
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private bool move(int dice,ref int position,ref int direction,Panel panel)
+        {
+            int nextPositin = dice + position;
+            int x = panel.Location.X;
+            int y = panel.Location.Y;
+            if (nextPositin > 100)
+            {
+                textBox1.Text = "Cannot Move";
+                Application.DoEvents();
+                Thread.Sleep(500);
+            }
+            else
+            {
+                for (int i = 0; i < dice; i++)
+                {
+
+                    if (position == 10 || position == 30 || position == 50 || position == 70 || position == 90)
+                    {
+                        y -= 60;
+                        direction *= -1;
+
+
+                    }
+                    else if (position == 20 || position == 40 || position == 60 || position == 80)
+                    {
+                        y -= 60;
+                        direction *= -1;
+                    }
+
+                    else
+                    {
+                        x += (84 * direction);
+                    }
+
+                    position++;
+                    panel.Location = new Point(x, y);
+                    Application.DoEvents();
+                    Thread.Sleep(150);
+
+                }
+
+            }
+            if (position == 100)
+            {
+                return true;
+            }
+            checkForStairs(ref position, ref direction, panel);
+            checkForSnakes(ref position, ref direction, panel);
+
+            return false;
+
+
+        }
+
+        private void checkForStairs(ref int position, ref int direction,Panel panel)
+        {     
+            int x=panel.Location.X;
+            int y = panel.Location.Y;
+            if (position == 8)
+            {
+                x += 84;
+                y -= (2 * 60);
+                panel.Location = new Point(x, y);
+
+                position = 29;
+                direction = 1;
+
+            }
+            else if (position == 22)
+            {
+                x -= 84;
+                y -= (4 * 60);
+                panel.Location = new Point(x, y);
+
+                position = 61;
+                direction = 1;
+            }
+            else if (position == 65)
+            {
+                x -= 84;
+                y -= (3 * 60);
+                panel.Location = new Point(x, y);
+
+                position = 97;
+                direction = -1;
+
+            }
+            else if (position == 54)
+            {
+                x += 84;
+                y -= (60);
+                panel.Location = new Point(x, y);
+                position = 68;
+                direction = 1;
+
+            }
+            else if (position == 72)
+            {
+                x -= 84;
+                y -= (2 * 60);
+                panel.Location = new Point(x, y);
+
+                position = 93;
+                direction = -1;
+
+            }
+        }
+        private void checkForSnakes(ref int position, ref int direction, Panel panel)
+        {
+            int x = panel.Location.X;
+            int y = panel.Location.Y;
+            if (position == 23)
+            {
+                x += 84;
+                y += 60;
+                panel.Location = new Point(x, y);
+                position = 17;
+                direction = -1;
+            }
+            else if(position == 99)
+            {
+                x += (2*84);
+                y += (7*60);
+                panel.Location = new Point(x, y);
+                position = 24;
+                direction = 1;
+            }
+            else if (position == 45)
+            {
+               
+                y += (4 * 60);
+                panel.Location = new Point(x, y);
+                position = 5;
+                direction = 1;
+            }
+            else if (position == 52)
+            {
+                x -= 84;
+                y += (2 * 60);
+                panel.Location = new Point(x, y);
+                position = 33;
+                direction = -1;
+            }
+            else if (position == 90)
+            {
+
+                y += (4 * 60);
+                panel.Location = new Point(x, y);
+                position = 50;
+                direction = 1;
+            }
+        }
+    }
+}
